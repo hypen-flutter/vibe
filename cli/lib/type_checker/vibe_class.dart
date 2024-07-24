4c33d1257b8fc5b43f3eb182c7c9747d315edbbe
wurikiji <wurikiji@gmail.com>
2024-07-24 23:26:32 +0900

    ✨ Add new typecheckers

---
+++ cli/lib/type_checker/vibe_class.dart
@@ -0,0 +1,10 @@
+import 'package:source_gen/source_gen.dart';
+
+const TypeChecker vibeWidget =
+    TypeChecker.fromUrl('package:vibe/widgets/widgets.dart#VibeWidget');
+
+const TypeChecker vibeStatefulWidget =
+    TypeChecker.fromUrl('package:vibe/widgets/widgets.dart#VibeStatefulWidget');
+
+const TypeChecker vibeWidgetState =
+    TypeChecker.fromUrl('package:vibe/widgets/widgets.dart#VibeWidgetState');
