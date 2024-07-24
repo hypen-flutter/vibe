24ae7c3a58ffcc82b7fc828f2d0191b00bd51462
wurikiji <wurikiji@gmail.com>
2024-07-24 21:00:42 +0900

    ✨ Add annotation checker

---
+++ cli/lib/annotations/state_annotations.dart
@@ -0,0 +1,16 @@
+import 'package:source_gen/source_gen.dart';
+
+const TypeChecker vibeAnnotation =
+    TypeChecker.fromUrl('package:vibe/annotations/annotations.dart#Vibe');
+const TypeChecker notVibeAnnotation =
+    TypeChecker.fromUrl('package:vibe/annotations/annotations.dart#NotVibe');
+const TypeChecker linkVibeAnnotation =
+    TypeChecker.fromUrl('package:vibe/annotations/annotations.dart#LinkVibe');
+const TypeChecker loaderAnnotation =
+    TypeChecker.fromUrl('package:vibe/annotations/annotations.dart#Loader');
+const TypeChecker selectVibeAnnotation =
+    TypeChecker.fromUrl('package:vibe/annotations/annotations.dart#SelectVibe');
+const TypeChecker streamVibeAnnotation =
+    TypeChecker.fromUrl('package:vibe/annotations/annotations.dart#StreamVibe');
+const TypeChecker vibeEffectAnnotation =
+    TypeChecker.fromUrl('package:vibe/annotations/annotations.dart#VibeEffect');
